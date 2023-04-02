b6e30f15862fc90ff61532f85c336f35f7eedecc
Mahir Rahman <rahmanm7@miamioh.edu>
2023-04-01 23:27:34 -0400

    Performed first API call to chatgpt with success

--- app.js
+++ app.js
@@ -3,17 +3,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion () {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "##### Translate this function  from Python into JavaScript\n### Python\n    \n    def sum():\n        return 1\n    \n### JavaScript",
+        temperature: 0,
+        max_tokens: 150,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["###"],
+    });
+    console.log(response.data.choices[0].text);
+}
 
 app.get('/', (req, res) => {
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/45056?key=${process.env.API_KEY}`;
-    axios.get(url)
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    });
+    // let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/45056?key=${process.env.API_KEY}`;
+    // axios.get(url)
+    // .then(function (response) {
+    //     console.log(response);
+    // })
+    // .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    // });
+    runCompletion();
     res.send('Hello World!')
 })
 
