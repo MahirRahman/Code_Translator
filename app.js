a2f5126d48165c6fcd520cc27496a08bd7cb3b03
Mahir Rahman <rahmanm7@miamioh.edu>
2023-04-02 16:51:14 -0400

    Added pseudo history db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
-const axios = require('axios');
+const db = require("./db.js")
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: process.env.API_KEY,
@@ -10,6 +10,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function runCompletion () {
+    searchText= "##### Translate this function  from Python into JavaScript\n### Python\n    \n    def sum():\n        return 1\n    \n### JavaScript"
+    
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "##### Translate this function  from Python into JavaScript\n### Python\n    \n    def sum():\n        return 1\n    \n### JavaScript",
@@ -21,18 +23,10 @@ async function runCompletion () {
         stop: ["###"],
     });
     console.log(response.data.choices[0].text);
+    db.insertDB(searchText, response.data.choices[0].text, new Date());
 }
 
 app.get('/', (req, res) => {
-    // let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/45056?key=${process.env.API_KEY}`;
-    // axios.get(url)
-    // .then(function (response) {
-    //     console.log(response);
-    // })
-    // .catch(function (error) {
-    //     // handle error
-    //     console.log(error);
-    // });
     runCompletion();
     res.send('Hello World!')
 })
