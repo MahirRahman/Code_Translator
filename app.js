4c4c3cad3e9c003b10b4e01ec7d1d71a252629d6
Mahir Rahman <rahmanm7@miamioh.edu>
2023-04-23 00:54:49 -0400

    Added sqlite login mechanism

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express = require('express')
-const app = express()
 require("dotenv").config({ path: "./.env" });
+
+const express = require('express')
 const path = require('path');
+const indexRouter = require('./routes/routes');
+const authRouter = require('./routes/auth');
+const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+
+const SQLiteStore = require('connect-sqlite3')(session);
+
+const app = express()
 const port = 3000
-app.use(express.json());
-app.use(require("./routes/routes.js"));
-app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, '../src')));
-console.log(path.join(__dirname, '/views'));
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.json());
+app.use(express.static(__dirname));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
+app.use('/', indexRouter);
+app.use('/', authRouter);
 
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,15 @@ const express = require('express');
 const routes = express.Router();
 const bcrypt = require('bcrypt');
 const db = require("../db.js")
+const authRouter = require('./auth');
 const { Configuration, OpenAIApi } = require("openai");
-const users = []; //local variable, change this to a JSON database
+// const users = []; //local variable, change this to a JSON database
 const configuration = new Configuration({
     apiKey: process.env.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const LocalStrategy = require('passport-local').Strategy;
-const flash = require('express-flash');
-const session = require('express-session');
-const passport = require('passport');
 
 routes.use(express.urlencoded({ extended: false }));
-routes.use(flash());
-routes.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-routes.use(passport.initialize())
-routes.use(passport.session())
 
 async function runCompletion (searchText, languageFrom, languageTo) {
     // searchText= "##### Translate this function  from Python into JavaScript\n### Python\n    \n"+searchText+"\n### JavaScript";
@@ -42,37 +31,6 @@ async function runCompletion (searchText, languageFrom, languageTo) {
     // db.insertDB(searchText, response.data.choices[0].text, new Date());
 }
 
-// Passport authentication
-function initializePassport(passport, getUserByName, getUserById)  {
-    const authenticateUser = async (name, password, done) => {
-        const user = getUserByName(name)
-        if (user == null) {
-            return done(null, false, { message: 'No user with that name'})
-        }
-        try {
-            if (await bcrypt.compare(password, user.password)) {
-                return done(null, user)
-            } else {
-                return done(null, false, { message: 'Password incorrect' })
-            }
-        } catch (e) {
-            return done(e)
-        }
-    }
-    passport.use(new LocalStrategy({ usernameField: 'name' },
-    authenticateUser))
-    passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => {
-        return done(null, getUserById(id))
-    })
-}
-
-initializePassport(
-    passport, 
-    name => users.find(user => user.name === name),
-    id => users.find(user => user.id === id)
-);
-
 routes.route("/").get(checkAuthenticated, (req, res) => {
     // runCompletion();
     res.render('index.ejs')
@@ -90,44 +48,11 @@ routes.get('/api/translate', checkAuthenticated, function(req,res) {
     // console.log(data);
 });
 
-routes.route("/history").get(checkAuthenticated, (req, res) => {
+routes.get("/history").get(checkAuthenticated, (req, res) => {
         db.display(function(x){
             res.render('history.ejs', {data: x});
          });
-    });
-
-routes.get('/wow', function(req,res) {
-    res.render('wow.ejs')
 });
-
-routes.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-routes.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-routes.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
-
-routes.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        res.redirect('/register')
-    }
-    console.log(users)
-})
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
@@ -135,13 +60,6 @@ function checkAuthenticated(req, res, next) {
     }
 
     res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        res.redirect('/')
-    }
-    next()
 }
 
 module.exports = routes;
